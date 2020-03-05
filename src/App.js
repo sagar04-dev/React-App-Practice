498eb3d39805c8cf71c61c61c6742063f833ee6b
sagar04-dev <59868686+sagar04-dev@users.noreply.github.com>
2020-03-05 13:28:01 +0530

    removed-unnecessary-code
    removed unnecessary code

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
-// import React from "react";
-// import "./styles.css";
-
-// export default function App() {
-//   return (
-//     <div className="App">
-//       <h1>Hello Sagar</h1>
-//       <h2>Welcome!</h2>
-//     </div>
-//   );
-// }
 import React, { Component } from "react";
-// import "./App.css";
 import Counter from "../container/counterContainer";
 import "./styles.css";
 
--- src/index.js
+++ src/index.js
@@ -1,22 +1,9 @@
-// import React from "react";
-// import ReactDOM from "react-dom";
-
-// import App from "./App";
-
-// const rootElement = document.getElementById("root");
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   rootElement
-// );
 import React from "react";
 import { render } from "react-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
 import reducer from "../reducer/index";
 import App from "./App";
-// import "./index.css";
 
 const store = createStore(
   reducer,
