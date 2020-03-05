6ba50369da4240c56a30c08ee27dbe0b4fc67a0e
sagar04-dev <59868686+sagar04-dev@users.noreply.github.com>
2020-03-05 13:13:51 +0530

    redux-increase-and-decrease-counter
    created one sample code of increase and decrease counter using redux

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+// import React from "react";
+// import "./styles.css";
+
+// export default function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello Sagar</h1>
+//       <h2>Welcome!</h2>
+//     </div>
+//   );
+// }
+import React, { Component } from "react";
+// import "./App.css";
+import Counter from "../container/counterContainer";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello Sagar</h1>
-      <h2>Welcome!</h2>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="content">
+          <header className="App-header">
+            <Counter />
+          </header>
+        </div>
+      </div>
+    );
+  }
 }
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   rootElement
+// );
 import React from "react";
-import ReactDOM from "react-dom";
-
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "../reducer/index";
 import App from "./App";
+// import "./index.css";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  rootElement
+  </Provider>,
+  document.getElementById("root")
 );
